26c2acafea05235b3a769fd43f0d3d1214b33223
James Singleton <jamesrsingleton1@gmail.com>
2023-08-27 15:09:29 -0700

    feat: setup sanity

---
+++ types/common.ts
@@ -0,0 +1,18 @@
+export interface ImageProps {
+  id: number
+  height: string
+  width: string
+  public_id: string
+  format: string
+  blurDataUrl?: string
+}
+
+export interface SharedModalProps {
+  index: number
+  images?: ImageProps[]
+  currentPhoto?: ImageProps
+  changePhotoId: (newVal: number) => void
+  closeModal: () => void
+  navigation: boolean
+  direction?: number
+}
